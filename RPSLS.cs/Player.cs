39cbe335853279617cdbe206fae8dabdd55a5b43
MegRyddner <44841685+MegRyddner@users.noreply.github.com>
2018-11-27 10:24:24 -0600

    Add files via upload

---
+++ RPSLS.cs/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS.cs
+{
+    public class Player
+    {//member variables
+        public int name;
+        public string chosenGesture;
+        List<string> gestures;
+        public Player(string chosenGesture, int name, string gestures)
+        {
+            this.name = name;
+            this.chosenGesture = chosenGesture;
+           
+        }
+        
+     
+     public void SetName()
+        {
+
+        }
+
+        public void ChooseGesture()
+        {
+            List<string> gestures = new List<string>();
+            gestures.Add("rock");
+            gestures.Add("paper");
+            gestures.Insert(1, "rock");
+            gestures.Insert(2, "paper");
+            gestures.Add("scissors");
+            gestures.Add("lizard");
+            gestures.Insert(3, "scissors");
+            gestures.Insert(4, "lizard");
+            gestures.Add("spock");
+            gestures.Insert(5, "spock");
+
+            foreach(string gesture in gestures)
+            {
+                Console.WriteLine(gesture);
+
+            }
+         
+        
+   
+        }
+
+         
+    }
+
+
+        
+
+
+    
+       
+
+
+
+
+
+
+
+    }
+       
+      
+
+        //methods SetName()  ChooseGesture()}
---
+++ RPSLS.cs/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS.cs
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //* Rock crushes Scissors 
+            //Cuts Paper
+            //Paper covers Rock
+            //Rock crushes Lizard
+            //Lizard poisons Spock
+            //Spock smashes Scissors
+            //Scissors decapitates Lizard
+            //Lizard eats Paper
+            //Paper disproves Spock
+            //Spock vaporizes Rock
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            
+                               
+
+        }
+    }
+}
